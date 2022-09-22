f3608cb7c4f3f78876688626a7c87cec989bfafe
notcat <41898000+notcat@users.noreply.github.com>
2022-09-22 04:23:03 -0700

    User input for username and pass from env or text

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
+import promptsync from 'prompt-sync';
+
 import 'dotenv/config';
 
-const username = process.env.USERNAME;
-if (!username) throw new Error('"USERNAME" env var is required!');
-const password = process.env.PASSWORD;
-if (!username) throw new Error('"PASSWORD" env var is required!');
+const prompt = promptsync({ sigint: true });
 
-console.log(username, password);
+let username: string = "";
+let password: string = "";
+const device: string = `CLI:${process.platform}`
+
+// Perhaps there is a cleaner way to do this? Maybe make it into a function?
+if (!process.env.USERNAME) {
+    while (username == "") {
+        username = prompt("What is the username? (300xxxxxx): ")
+    }
+} else {
+    console.log("Username supplied from env variables.")
+    username = process.env.USERNAME;
+}
+
+if (!process.env.PASSWORD) {
+    while (password == "") {
+        password = prompt("What is the password?: ")
+    }
+} else {
+    console.log("Password supplied from env variables.")
+    password = process.env.PASSWORD;
+}
+
+console.log(`Logging in with student number ${username}`);
+
