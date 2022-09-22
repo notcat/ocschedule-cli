066bda865d4c762c74c093ab224001fa5fd1e397
notcat <41898000+notcat@users.noreply.github.com>
2022-09-22 09:15:03 -0700

    change reserved env var "username" to "ocusername" and fix package.json

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,18 @@ import * as logindevice from './requests/loginDevice.js';
 
 const prompt = promptsync({ sigint: true });
 
-let username: string = "";
+let ocusername: string = "";
 let password: string = "";
 const deviceName: string = `CLI:${process.platform}`
 
 // Perhaps there is a cleaner way to do this? Maybe make it into a function?
-if (!process.env.USERNAME) {
-    while (username == "") {
-        username = prompt("What is the username? (300xxxxxx): ")
+if (!process.env.OCUSERNAME) {
+    while (ocusername == "") {
+        ocusername = prompt("What is the ocusername? (300xxxxxx): ")
     }
 } else {
-    console.log("Username supplied from env variables.")
-    username = process.env.USERNAME;
+    console.log("ocusername supplied from env variables.")
+    ocusername = process.env.OCUSERNAME;
 }
 
 if (!process.env.PASSWORD) {
@@ -32,16 +32,18 @@ if (!process.env.PASSWORD) {
     password = process.env.PASSWORD;
 }
 
-console.log(`Logging in with student number ${username}`);
+console.log(ocusername, password)
 
-rdevice.RegisterDevice(username, password, deviceName).then((device: rdevice.RegisteredDevice) => {
+console.log(`Logging in with student number ${ocusername}`);
+
+rdevice.RegisterDevice(ocusername, password, deviceName).then((device: rdevice.RegisteredDevice) => {
     console.log(device);
 
-    logindevice.LoginDevice(username, device.deviceID).then(async (cookie: logindevice.AuthCookie) => {
+    logindevice.LoginDevice(ocusername, device.deviceID).then(async (cookie: logindevice.AuthCookie) => {
         //console.log(cookie);
 
         // TODO: make it so you can select the date you want to see
-        let url = `https://webapps-5.okanagan.bc.ca/ok/OCMobile/api/Schedule/GetScheduleByDate?studentID=${username}&startDate=2022-09-21T01%3A06%3A24&endDate=2022-09-22T01%3A06%3A24&page=1&start=0&limit=25`
+        let url = `https://webapps-5.okanagan.bc.ca/ok/OCMobile/api/Schedule/GetScheduleByDate?studentID=${ocusername}&startDate=2022-09-21T01%3A06%3A24&endDate=2022-09-22T01%3A06%3A24&page=1&start=0&limit=25`
 
         const response = await fetch(url, {
             method: 'GET',
